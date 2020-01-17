166b9576b40d73f772afbd1c7b2b3659efb6751e
yameste <yameste@yahoo.co.uk>
2020-01-17 08:24:34 +0000

    before the installation of a bootstrap date picker

--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,3 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('checks', 'CheckController@store');
 
-
-
-// Route::get('skills', function () {
-
-//     return ['Laravel', 'Vue', 'PHP', 'Javascript', 'Tooling'];
-
-// });
